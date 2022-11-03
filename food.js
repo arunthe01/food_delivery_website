faa4c7e4f0efeaf9cd44b0022d16108789630de8
arun emmidi <djdjdjaaa999@gmail.com>
2022-11-03 20:43:43 +0530

    modifications done

--- food.js
+++ food.js
@@ -1,16 +1,29 @@
 
 
 
-        function alertify() {
+        function OrderNow() {
 
             var t = document.getElementById("foods");
-            if(document.getElementById("name").value == "" || document.getElementById("email").value == "" || document.getElementById("contact").value == "" || document.getElementById("adress").value == "")
+           
+            if( document.getElementById("foods").value == "na"  ||document.getElementById("name").value == "" || document.getElementById("email").value == "" || document.getElementById("contact").value == "" || document.getElementById("adress").value == "")
                 alert("fill all the fields");
 
-            else
+            else{
+               
                 alert(" Your "+t.value+" Order Placed");
-
-
+            }
         }
+
+
+        function Clear(){
+
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("contact").value = "";
+            document.getElementById("adress").value = "";
+        }
+
+
+
 
 
